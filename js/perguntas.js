908449182e1fa3643962173b02b1fa8adc352ad8
engolecacete <129764887+engolecacete@users.noreply.github.com>
2024-08-21 12:18:24 -0300

    a

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+const perguntas = [
+    {
+        enunciado: "O que o personagem folclórico curupira fazia na selva brasileira ?",
+        alternativas: [
+            {
+                texto: "Buscava por pessoas perdidas e desamparadas para servir de refeição ",
+                afirmacao: [
+                    "Você não entende de folclore brasileiro, busque estudar mais",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Protegia a selva de invasores e buscava por paz no meio ambiente",
+                afirmacao: [
+                    "Você aparenta saber bastante de folclore basileiro, continue assim",
+                    "afirmacao 2"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto: "Alternativa 2 da pergunta 2",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto:"Alternativa 1 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            },
+            {
+                texto:"Alternativa 2 da pergunta 3",
+                afirmacao: [
+                    "afirmacao 1",
+                    "afirmacao 2"
+                    ]
+            }
+            
+        ]
+    },
+];
